a746b7c325703940e2755d136bb5502654003f92
Alain Hélaïli <helaili@github.com>
2017-03-17 22:15:02 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <topbar></topbar>
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Topbar from '@/components/Topbar'
+
+export default {
+  name: 'app',
+  components: {
+    Topbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
