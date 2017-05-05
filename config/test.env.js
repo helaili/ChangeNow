74e655f9ebbec6f14faff729553c58f371afbb54
Alain Hélaïli <helaili@github.com>
2017-05-05 22:27:41 +0200

    config test db

--- config/test.env.js
+++ config/test.env.js
@@ -2,5 +2,13 @@ var merge = require('webpack-merge')
 var devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  db: {
+    connectionLimit: 10,
+    acquireTimeout: 10000,
+    host: '127.0.0.1',
+    user: 'travis',
+    password: '',
+    database: 'ChangeNow'
+  }
 })
