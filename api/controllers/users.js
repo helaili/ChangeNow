6f5914d521aed2ea948b4be56a91ed35624cfd77
Alain Hélaïli <helaili@github.com>
2017-05-15 21:58:59 +0200

    experimenting during migration

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const science = require('../scientist/console');
+
 var flipper = require('../helpers/flipper');
 var fs = require('fs');
+
 
 module.exports = {
   getAll: getAll
@@ -12,54 +15,109 @@ function sleep(ms) {
 }
 
 function getAll(req, res) {
-  req.dbPool.getConnection(function(err, connection) {
-    if (err) {
-      console.error('Error retrieving a connection from the pool: ' + err.stack)
-      return res.status(503).json({'message' : 'Database error'})
-    }
+  flipper.isEnabled('newUserSchema', null, function (error, newUserSchemaFlipperFlag) {
+		if(error) {
+			return res.status(503).json({'message' : error})
+		} else {
+      if(newUserSchemaFlipperFlag.enabled) {
+        console.log('new schema')
 
-    flipper.isEnabled('newUserSchema', null, function (error, result) {
-  		if(error) {
-  			return res.status(503).json({'message' : error})
-  		} else {
-        if(result.enabled) {
-          console.log('new schema')
-
-          // Has the cut-over been triggered yet?
-          if(fs.existsSync('/tmp/users.postpone.flag')) {
-            try {
-              // Triggering the cut-over
-              fs.unlinkSync('/tmp/users.postpone.flag')
-              // Waiting for the cut-over to complete
-              while(fs.existsSync('/tmp/users.test.sock')) {
-              }
-            } catch (err) {
-              //Don't care
+        // Has the cut-over been triggered yet?
+        if(fs.existsSync('/tmp/users.postpone.flag')) {
+          try {
+            // Triggering the cut-over
+            fs.unlinkSync('/tmp/users.postpone.flag')
+            // Waiting for the cut-over to complete
+            while(fs.existsSync('/tmp/users.test.sock')) {
             }
+          } catch (err) {
+            //Don't care
           }
+        }
 
-          connection.query('SELECT firstname, lastname, email FROM users', function (error, results, fields) {
-            if (err) {
-              console.error('Error retrieving users from the database: ' + err.stack)
-              return res.status(503).json({'message' : 'Database error'})
+        getAllUsersNew(req.dbPool, 'users').then(function(result) {
+          return res.json(result);
+        }, function(error) {
+          return res.status(503).json(error);
+        });
+
+      } else { // newUserSchemaFlipperFlag.enabled == false
+        console.log('old schema')
+
+        var experimentResult = science('Get All Users', (experiment) => {
+          experiment.async(true)
+          experiment.context({ 'name':  'Get All Users'});
+          experiment.use(() => getAllUsersOld(req.dbPool))
+          // Experimenting with the temp gh-ost db
+          experiment.try(() => getAllUsersNew(req.dbPool, '_users_gho'))
+
+
+          experiment.compare((a, b) => {
+            if(JSON.stringify(a) === JSON.stringify(b)) {
+              return true
             } else {
-              connection.release()
-              return res.json(results)
+              return false
             }
           })
-        } else {
-          console.log('old schema')
-          connection.query('SELECT first as firstname, last as lastname, email FROM users', function (error, results, fields) {
-            if (err) {
-              console.error('Error retrieving users from the database: ' + err.stack)
-              return res.status(503).json({'message' : 'Database error'})
+
+        })
+
+        experimentResult.then(function(result) {
+          return res.json(result);
+        }, function(error) {
+          return res.status(503).json(error);
+        })
+      }
+		}
+	})
+}
+
+function getAllUsersOld(dbPool) {
+  return new Promise(
+    (resolve, reject) => {
+      dbPool.getConnection(function(connectionError, connection) {
+        if (connectionError) {
+          console.error('Error retrieving a connection from the pool: ' + err.stack)
+          return res.status(503).json({'message' : 'Database error'})
+        } else {
+          connection.query('SELECT first as firstname, last as lastname, email FROM users',  (error, results, fields) => {
+            if (error) {
+              console.error('Error retrieving users from the database: ' + error.stack)
+              reject({
+                'message' : 'Database error'
+              })
             } else {
               connection.release()
-              return res.json(results)
+              resolve(results)
             }
           })
         }
-  		}
-  	})
-  })
+      })
+    }
+  )
+}
+
+function getAllUsersNew(dbPool, table) {
+  return new Promise(
+    (resolve, reject) => {
+      dbPool.getConnection(function(connectionError, connection) {
+        if (connectionError) {
+          console.error('Error retrieving a connection from the pool: ' + err.stack)
+          return res.status(503).json({'message' : 'Database error'})
+        } else {
+          connection.query('SELECT firstname, lastname, email FROM ' + table,  (error, results, fields) => {
+            if (error) {
+              console.error('Error retrieving users from the database: ' + error.stack)
+              reject({
+                'message' : 'Database error'
+              })
+            } else {
+              connection.release()
+              resolve(results)
+            }
+          })
+        }
+      })
+    }
+  )
 }
